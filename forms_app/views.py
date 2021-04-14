0d8135f2c0035dcb1ea572a4354ebdaa13167f5e
fralue02 <71756347+fralue02@users.noreply.github.com>
2021-04-14 10:15:26 +0200

    l24

--- forms_app/views.py
+++ forms_app/views.py
@@ -1,8 +1,24 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from .forms import FormContatto
 
 def contatti(request):
-    form=FormContatto()
+    if request.method=='POST':
+     form=FormContatto(request.POST)
+
+     if form.is_valid():
+         print('il form è valido')
+         print('NOME:',form.cleaned_data['nome'])
+         print('COGNOME:',form.cleaned_data['cognome'])
+         print('EMAIL:',form.cleaned_data['email'])
+         print('CONTENUTO:',form.cleaned_data['contenuto'])
+
+         return HttpResponse('<h1>grazie per averci contattato!</h1>')
+
+    else:
+        form=FormContatto()    
+
+
     context = {"form":form}
     return render(request,"contatto.html",context)
 
--- libreria/urls.py
+++ libreria/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import LibroListCVB,AutoreDetailCVB
+app_name='libreria'
 urlpatterns = [
     path('', LibroListCVB.as_view(), name='lista_libri'),
     path('autore/<int:pk>/', AutoreDetailCVB.as_view(), name='profilo_autore')
